31844dd04712b5e972c271b5afbc4b1b48a38186
fernandesalan23 <fernandes.alan23@gmail.com>
2020-04-17 03:50:27 +0530

    Templates, Interpolation, Directives, Data Binding and Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-  <div><h1>My first HTML</h1></div>
+  <div><h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>
   `
 })
 
 export class AppComponent{
-  pageTitle: string = 'Alan';
+  pageTitle: string = 'Acme Product Management';
 }
