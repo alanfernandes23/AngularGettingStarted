6698c3e45714aa293d06368f58e634f89817777e
fernandesalan23 <fernandes.alan23@gmail.com>
2020-04-19 23:09:10 +0530

    modules, cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <a class='navbar-brand'>{{pageTitle}}</a>
     <ul class='navbar nav-pills'>
       <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
-      <li><a class='nav-link' [routerLink]="['/products']"> Product List</a></li>
+      <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
     </ul>
   </nav>
   <div class='container'>
